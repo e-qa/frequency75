f82fa008aea7f3b2dc57ed08df710b375ec1dda8
Ali <eliqarayev835@gmail.com>
2024-05-09 15:39:36 +0400

    add tailwind and basic class structure

---
+++ main.js
@@ -0,0 +1,78 @@
+import * as Tone from "tone";
+import "./main.css";
+
+const input = document.getElementById("file");
+
+class BeatPattern {
+  constructor() {
+    this.lineCount = 1;
+  }
+  createLine(bgClass) {
+    const div = document.createElement("div");
+    div.className = `${bgClass}`;
+    div.innerText = this.lineCount;
+    div.setAttribute("data-id", this.lineCount);
+    div.addEventListener("click", () => {
+      const dataId = div.getAttribute("data-id");
+      soundSampler.playSample(dataId);
+    });
+
+    document.body.appendChild(div);
+    this.lineCount++;
+  }
+}
+
+class SoundSampler {
+  constructor() {
+    this.samples = [];
+    this.id = null;
+    this.static = 1;
+  }
+
+  newSample(url) {
+    this.samples.push({
+      id: this.static,
+      sampler: new Tone.Sampler({
+        urls: {
+          ["C2"]: url,
+        },
+      }).toDestination(),
+    });
+    this.static++;
+  }
+  getID(id) {
+    this.id = id;
+  }
+  async playSample(dataID) {
+    this.samples.map((s) => {
+      console.log(s.id === dataID);
+
+      if (s.id == dataID) {
+        console.log("first");
+
+        s.sampler.triggerAttack("C2");
+      }
+    });
+  }
+}
+
+const beatPattern = new BeatPattern();
+const soundSampler = new SoundSampler();
+
+const createPattern = document.getElementById("create-pattern");
+
+createPattern.addEventListener("click", () => {
+  beatPattern.createLine("w-20 m-2 bg-green-600 play");
+  soundSampler.getID(beatPattern.lineCount);
+});
+
+input.addEventListener("change", function () {
+  let file = input.files[0];
+  let wavBlobURL = URL.createObjectURL(file);
+  soundSampler.newSample(wavBlobURL);
+});
+
+const play = document.getElementById("play");
+play.addEventListener("click", () => {
+  soundSampler.playSample();
+});
