3393cec2905fd678fb769c308747a60c1384ca1a
Ali <eliqarayev835@gmail.com>
2024-05-26 03:24:42 +0400

    add dynamic color to the pattern

--- src/js/createPattern.js
+++ src/js/createPattern.js
@@ -1,19 +1,28 @@
 class CreatePattern {
   #patternLength = 16;
   element;
-
   constructor(patternName) {
     this.patternName = patternName;
     this.element = this.newPattern();
   }
 
-  createCheckbox(index) {
+  createCheckbox(index, color) {
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-
+    let colorVar = {
+      'bg-fuchsia-600': 'checked:bg-fuchsia-600',
+      'bg-purple-600': 'checked:bg-purple-600',
+      'bg-sky-600': 'checked:bg-sky-600',
+      'bg-emerald-600': 'checked:bg-emerald-600',
+      'bg-amber-600': 'checked:bg-amber-600',
+      'bg-red-600': 'checked:bg-red-600',
+      'bg-green-600': 'checked:bg-green-600',
+    };
+    checkbox.classList.add(`${colorVar[color]}`);
+    6;
     if (index === 0) {
       checkbox.type = 'text';
-      checkbox.classList.add('bg-red-900', 'border-b-2', 'border-red-700', 'w-20');
+      checkbox.classList.add(`${color}`, 'border-b-2', 'border-red-700', 'w-20', 'checked:bg-blue-800');
       checkbox.value = this.patternName;
       checkbox.disabled = true;
     }
@@ -22,11 +31,13 @@ class CreatePattern {
   }
 
   newPattern() {
+    const colors = ['bg-fuchsia-600', 'bg-purple-600', 'bg-sky-600', 'bg-emerald-600', 'bg-amber-600', 'bg-red-600', 'bg-green-600'];
+    let color = colors[Math.floor(Math.random() * colors.length)];
+
     const div = document.createElement('div');
     div.classList.add('flex');
-
     for (let index = 0; index <= this.#patternLength; index++) {
-      const checkbox = this.createCheckbox(index);
+      const checkbox = this.createCheckbox(index, color);
       div.appendChild(checkbox);
     }
 
